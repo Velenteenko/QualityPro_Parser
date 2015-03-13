05a0d4dc481b936ffab593d1f700745d230539fc
Dmirti Velenteenko <dimonwilli@gmail.com>
2015-03-13 12:29:26 +0200

    Add new class read dbf.

--- src/vza/vaz/readdbf/MainRead.java
+++ src/vza/vaz/readdbf/MainRead.java
@@ -9,9 +9,12 @@ package vza.vaz.readdbf;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -20,47 +23,94 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.io.IOUtils;
+
+import com.hexiong.jdbf.DBFReader;
+import com.hexiong.jdbf.JDBFException;
+
 import jcifs.smb.SmbFile;
 import jcifs.smb.SmbFileInputStream;
 import jcifs.smb.SmbFileOutputStream;
 import vza.vaz.XmlSettings.ParseXmlSettings;
-import nl.knaw.dans.common.dbflib.CorruptedTableException;
-//import nl.knaw.dans.common.dbflib.Field;
-import nl.knaw.dans.common.dbflib.IfNonExistent;
-import nl.knaw.dans.common.dbflib.Record;
-import nl.knaw.dans.common.dbflib.Table;
+//import nl.knaw.dans.common.dbflib.CorruptedTableException;
+////import nl.knaw.dans.common.dbflib.Field;
+//import nl.knaw.dans.common.dbflib.IfNonExistent;
+//import nl.knaw.dans.common.dbflib.Record;
+//import nl.knaw.dans.common.dbflib.Table;
 
 public class MainRead {
 
-	private static File stream2file(SmbFileInputStream in) throws IOException {
-		final File tempFile = File.createTempFile("stream2file", ".tmp");
-		tempFile.deleteOnExit();
-		try (SmbFileOutputStream out = new SmbFileOutputStream(tempFile)) {
-			
-		} 
-		return null;
+	private static String stream2file(String path) throws IOException {
+		 SmbFile remoteFile = new SmbFile(path);
+		 OutputStream os = new FileOutputStream("stream2file.tmp");
+		 InputStream is = remoteFile.getInputStream();
+		 int bufferSize = 5096;
+		 byte[] b = new byte[bufferSize];
+		 int noOfBytes = 0;
+		  while( (noOfBytes = is.read(b)) != -1 )
+		    {
+		      os.write(b, 0, noOfBytes);
+		    }
+		 os.close();
+		 is.close();
+		 return "stream2file.tmp";
 	}
 	
 	
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException, IOException {
+    	
+    	ReadDbf dbf = new ReadDbf();
+    	
         String db_path = "/home/velenteenko/WorkDir/m1.DBF";
-        
-        HashMap<String, ArrayList<String>> mapPath = new HashMap<String, ArrayList<String>>(ParseXmlSettings.readXML("path", "name", "p"));
+        HashMap<String, ArrayList<String>> mapPath = new HashMap<String, ArrayList<String>>(ParseXmlSettings.readXML("field", "name", "p"));
         for (Map.Entry<String, ArrayList<String>> entryset : mapPath.entrySet()) {
 			String value = entryset.getValue().get(0);
+			if(entryset.getKey().equals("basePath")){
 			db_path = value;
+			}
 		}
        
-        try {
-			SmbFile file = new SmbFile(db_path);
-		} catch (MalformedURLException e1) {
+        DBFReader dbfreader;
+		try {
+			dbfreader = new DBFReader("stream2file.DBF");
+			 //DBFReader dbfreader = new DBFReader("E:\\hexiongshare\\test.dbf");
+	        int j=0;
+	        for (j = 0; j < dbfreader.getFieldCount(); j++) {
+	          System.out.print(dbfreader.getField(j).getName()+"  ");
+	        }
+	        System.out.print("\n");
+	        for(j = 0; dbfreader.hasNextRecord(); j++)
+	        {
+	            Object aobj[] = dbfreader.nextRecord(Charset.forName("CP1251"));
+//	            for (int k=0; k < aobj.length; k++)
+	              System.out.print(aobj[2]+"  |  ");
+	            System.out.print("\n");
+	        }
+
+	        System.out.println("Total Count: " + j);
+		} catch (JDBFException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+      
+
         
-        Table table = new Table(new File(db_path),"CP1251");
-        try {
-            table.open(IfNonExistent.ERROR);
+//        File tableFile = null;
+//        try {
+//			SmbFile file = new SmbFile(db_path);
+//			
+//			tableFile = stream2file(file);
+//		} catch (IOException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+        
+        
+        
+//        Table table = new Table(new File(stream2file(db_path)),"CP1251");
+////        Table table = new Table(new File("stream2file.DBF"), "CP1251");
+//        try {
+//            table.open(IfNonExistent.ERROR);
 //            List<Field> fields = table.getFields();
 //            for (Field field : fields)
 //            {
@@ -71,38 +121,38 @@ public class MainRead {
 //                System.out.println();
 //            }
 
-        Iterator<Record> iterator = table.recordIterator();
-
-         List<String> old = new ArrayList<String>();
-           // old = new LinkedList<String>();
-         Set<String> newset = new HashSet<String>();
-
-            while (iterator.hasNext())
-            {
-                Record record = iterator.next();
-               String ch = record.getStringValue("MN");
-                if (ch != null) {
-                    // System.out.println(ch);
-                    // System.out.println("\n");
-                    old.add(ch);
-                    newset.add(ch);
-                }
-            }
-            String st = old.get(2);
-            int len = st.length();
-
-            System.out.println("Duplicates:");
-
-            System.out.println("Array: "+old.size()+" Len: "+len);
-            System.out.println("HashSet: "+newset.size());
-            int deff = old.size() - newset.size();
-            System.out.println("Deff: "+deff);
+//        Iterator<Record> iterator = table.recordIterator();
+//
+//         List<String> old = new ArrayList<String>();
+//           // old = new LinkedList<String>();
+//         Set<String> newset = new HashSet<String>();
+//
+//            while (iterator.hasNext())
+//            {
+//                Record record = iterator.next();
+//               String ch = record.getStringValue("MN");
+//                if (ch != null) {
+//                    // System.out.println(ch);
+//                    // System.out.println("\n");
+//                    old.add(ch);
+//                    newset.add(ch);
+//                }
+//            }
+//            String st = old.get(2);
+//            int len = st.length();
+//
+//            System.out.println("Duplicates:");
+//
+//            System.out.println("Array: "+old.size()+" Len: "+len);
+//            System.out.println("HashSet: "+newset.size());
+//            int deff = old.size() - newset.size();
+//            System.out.println("Deff: "+deff);
             
-            int i=0;
-            for (String string : newset) {
-            	i++;
-				System.out.println(i+": "+string);
-			}
+//            int i=0;
+//            for (String string : newset) {
+//            	i++;
+//				System.out.println(i+": "+string);
+//			}
 //
 //            String regex = "(\\w+(ГОСТ)+\\w*)";//(\\w+)ДСТУ(\\w+)ОТС(\\w+)ТУ(\\w+)*" ;
 //            String s = "Лист 1.5х700х1400 ГОСТ 19904-90 / 17Х18Н9Н ТУ 14-1-2186-77";
@@ -117,19 +167,19 @@ public class MainRead {
 //            System.out.println("2: " + m2.group(2));
 //        }
 
-    }catch (IOException ex){
-            System.out.println(ex.getMessage());
-        }
-        catch(CorruptedTableException ex){
-            System.out.println(ex.getMessage());
-        }
-
-        finally {
-            try {
-                table.close();
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
- }
+//    }catch (IOException ex){
+//            System.out.println(ex.getMessage());
+//        }
+//        catch(CorruptedTableException ex){
+//            System.out.println(ex.getMessage());
+//        }
+//
+//        finally {
+//            try {
+//                table.close();
+//            } catch (IOException e) {
+//                System.out.println(e.getMessage());
+//            }
+// }
     }
 }
--- src/vza/vaz/readdbf/ReadDbf.java
+++ src/vza/vaz/readdbf/ReadDbf.java
@@ -1,14 +1,130 @@
 package vza.vaz.readdbf;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+
+import com.hexiong.jdbf.DBFReader;
+import com.hexiong.jdbf.JDBFException;
+
+import vza.vaz.XmlSettings.ParseXmlSettings;
+import jcifs.smb.SmbFile;
 
 public class ReadDbf {
 
+	private String pathToBase;
+	private String tmpFileName;
+	private String charSetName;
+	private int collumnIndex;
+	private ArrayList<String> countTags;
+	private int countLines;
+	
+	public ReadDbf()
+	{
+		readConfig();
+		try {
+			stream2file();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.collumnIndex = 0;
+		this.countLines = 0;
+	}
+	
 	private HashSet<String> collectionRows;
 	
-	public static HashSet<String> getCollectionRows()
+	private void readConfig()
 	{
-		
+		String key = null;
+		ArrayList<String> value = null;
+        HashMap<String, ArrayList<String>> mapPath = new HashMap<String, ArrayList<String>>(ParseXmlSettings.readXML("field", "name", "p"));
+        for (Map.Entry<String, ArrayList<String>> entryset : mapPath.entrySet()) {
+        	key = entryset.getKey();
+        	value = new ArrayList<String>(entryset.getValue());
+        	switch (key) {
+			case "basePath":
+				this.pathToBase = value.get(0);
+				break;
+			case "tempFile":
+				this.tmpFileName = value.get(0);
+				break;
+			case "charset":
+				this.charSetName = value.get(0);
+				break;
+			case "columnIndex":
+				this.collumnIndex = Integer.parseInt(value.get(0));
+				break;
+			}
+		}
 	}
+	
+	private void stream2file() throws IOException
+	{
+		SmbFile remoteFile = new SmbFile(pathToBase);
+		 OutputStream os = new FileOutputStream(tmpFileName);
+		 InputStream is = remoteFile.getInputStream();
+		 int bufferSize = 5096;
+		 byte[] b = new byte[bufferSize];
+		 int noOfBytes = 0;
+		  while( (noOfBytes = is.read(b)) != -1 )
+		    {
+		      os.write(b, 0, noOfBytes);
+		    }
+		 os.close();
+		 is.close();
+	}
+
+	public HashSet<String> getRows() {
+		
+		DBFReader dbfreader;
+		this.collectionRows = new HashSet<String>();
+		try {
+			dbfreader = new DBFReader(tmpFileName);
+//	        int j=0;
+//	        for (j = 0; j < dbfreader.getFieldCount(); j++) {
+//	          System.out.print(dbfreader.getField(j).getName()+"  ");
+//	        }
+//	        System.out.print("\n");
+			int i = 0;
+	        for( i = 0; dbfreader.hasNextRecord(); i++)
+	        {
+	            Object aobj[] = dbfreader.nextRecord(Charset.forName("CP1251"));
+	            for (int k=0; k < aobj.length; k++)
+	            	 this.collectionRows.add((String) aobj[this.collumnIndex]);
+	        }
+
+//	        System.out.println("Total Count: " + i);
+	        this.countLines = i;
+		} catch (JDBFException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return collectionRows;
+	}
+	
+	
+	public void setRows(HashSet<String> collectionRows) {
+		this.collectionRows = collectionRows;
+	}
+
+	public int getCountLines() {
+		return countLines;
+	}
+
+	public int getCollumnIndex() {
+		return collumnIndex;
+	}
+
+	public void setCollumnIndex(int collumnIndex) {
+		this.collumnIndex = collumnIndex;
+	}
+	
 	
 }
